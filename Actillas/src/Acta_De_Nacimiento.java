e7d7d11c0e299a5e2d81c2daff9e07d261d6cf03
Taric2 <jorgealbertomq11@gmail.com>
2024-05-05 13:54:22 -0700

    prueba

---
+++ Actillas/src/Acta_De_Nacimiento.java
@@ -0,0 +1,11 @@
+public class Acta_De_Nacimiento {
+
+    public Acta_De_Nacimiento(Niño Resgistrado, InfoDoc Info, Padres Padre, Padres Madre) {
+        
+    }
+
+    public Acta_De_Nacimiento(Niño Registrado, InfoDoc Info, Padres Soltero) {
+
+    }
+
+}
